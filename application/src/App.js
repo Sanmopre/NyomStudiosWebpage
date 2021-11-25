673c872fedbc0a0efd0e5997870c73ba382d64ab
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2021-11-25 21:21:17 +0100

    Firebase config

--- application/src/App.js
+++ application/src/App.js
@@ -1,79 +1,21 @@
-
 import './App.css';
-import firebase from "firebase/compat/app"
-import "firebase/compat/auth"
-import "firebase/compat/firestore"
+import React, { useEffect } from 'react';
+import { collection, getDocs} from 'firebase/firestore'
+import db from './firebase/firebaseConfig';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCpzOFtkgy5fFdsgBtYfF8-zSbsqVEjh_k",
-  authDomain: "react-firebase-applicati-44951.firebaseapp.com",
-  projectId: "react-firebase-applicati-44951",
-  storageBucket: "react-firebase-applicati-44951.appspot.com",
-  messagingSenderId: "897941785637",
-  appId: "1:897941785637:web:ac81d2d8aa3ed63ca383cc",
-  measurementId: "G-46RF3CEVS0"
-});
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-
-function App() {
-
-  const [user] = useAuthState(auth);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header> 
-      <section>
-        {user ? <ChatRoom /> : <SignIn/>}
-      </section>
-    </div>
-  );
-}
-
-function SignIn()
-{
-const signInWithGoogle = () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  auth.signInWithPopup(provider);
-}
+const App = () => {
+  //HOOK
+  useEffect(() => {
+    const obtenerDatos = async()=> {
+      const datos = await getDocs(collection(db,'usuarios'));
+      return datos.docs[0].data().nombre;
+    }
+    console.log(obtenerDatos());
+  },[]);
   return(
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <h1>Firebase Application!</h1>
   )
-}
-
-function SignOut()
-{
-return auth.currentUser && (
-  <button onClick={() => auth.signOut()}>Sign Out</button>
-)
-}
-
-function ChatRoom()
-{
-const messageRef = firestore.collection('messages');
-const query = messageRef.orderBy('createdAt').limit(30);
-
-const [messages] = useCollectionData(query, {idField: 'id'});
-
-return(
-  <>
-  <div>
-    {messages && messages.map(msg=> <ChatMessage key={msg.id} message = {msg}/>)}
-  </div>
-  </>
-)
-}
-
-function ChatMessage(props)
-{
-  const {text, uid} = props.message;
-
-  return <p>{text}</p>
 }
 
 export default App;
