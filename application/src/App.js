e70860fba0d4723f1ebf7f883b6042d0735c1fb7
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2022-01-19 16:38:15 +0100

    update

--- application/src/App.js
+++ application/src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
 import React, { useEffect } from 'react';
-import { collection, getDocs} from 'firebase/firestore'
-import db from './firebase/firebaseConfig';
 
 
 const App = () => {
-  //HOOK
-  useEffect(() => {
-    const obtenerDatos = async()=> {
-      const datos = await getDocs(collection(db,'usuarios'));
-      return datos.docs[0].data().nombre;
-    }
-    console.log(obtenerDatos());
-  },[]);
-  return(
-    <h1>Firebase Application!</h1>
-  )
+return(
+    <h1>Nyom studios webpage</h1>
+)
 }
 
 export default App;
